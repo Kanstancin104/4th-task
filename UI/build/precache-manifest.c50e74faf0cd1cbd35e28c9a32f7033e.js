11831621c76aee29f06c31f9017f7d5071d78b57
Kanstancin Vienhura <vengurakonstant@gmail.com>
2022-12-02 21:31:08 +0100

    frontentd changes

---
+++ UI/build/precache-manifest.c50e74faf0cd1cbd35e28c9a32f7033e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1fbbce1b52088f365af",
+    "url": "/static/css/main.ea852726.chunk.css"
+  },
+  {
+    "revision": "a1fbbce1b52088f365af",
+    "url": "/static/js/main.a1fbbce1.chunk.js"
+  },
+  {
+    "revision": "d3f6dc00690bec69af8c",
+    "url": "/static/css/1.f6ae9217.chunk.css"
+  },
+  {
+    "revision": "d3f6dc00690bec69af8c",
+    "url": "/static/js/1.d3f6dc00.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c26bd9f558b0dd3584ca2ea86122159",
+    "url": "/index.html"
+  }
+];
